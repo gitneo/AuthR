df5a94e5a43078d57453c658fb6bf483109ec07d
ogo okafor <ogo.okafor@turnkeyafrica.com>
2022-08-21 14:10:24 +0100

    Define app spring boot application

---
+++ basic-login-service/src/main/java/com/cron/authr/AuthRApplication.java
@@ -0,0 +1,11 @@
+package com.cron.authr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthRApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthRApplication.class, args);
+    }
+}
